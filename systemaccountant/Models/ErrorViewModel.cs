bfa45f5c9323b2b2225e61fd1608f5923df0f4be
mazenhzm <hzmmazn4@gmail.com>
2024-09-08 02:56:43 +0300

    Add files via upload

---
+++ systemaccountant/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace systemaccountant.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
